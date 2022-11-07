9519894408b6cbe08cbf251424f7b74822be5d16
jeremymcu <10890402@me.mcu.edu.tw>
2022-11-07 17:03:06 +0800

    try 1.2 對

--- src/Project5/Main.java
+++ src/Project5/Main.java
@@ -3,7 +3,6 @@ package Project5;
 import java.util.Arrays;
 import java.util.Stack;
 
-
 public class Main {
     void bubbleSort(int[] nums, int len){
         Stack<Integer> stack1 = new Stack<>();
@@ -13,12 +12,11 @@ public class Main {
             stack1.push(num);
         }
 
-        stack2.push(stack1.pop());
-
-        for (int i=0; i<len; i+=1){
-            for (int j=0; j<len-i; j+=1){
+        for (int i=0; i<len-1; i+=1){
+            stack2.push(stack1.pop());
+            for (int j=0; j<len-i-1; j+=1){
                 while(!stack1.isEmpty()){
-                    if(stack1.peek()<stack2.peek()){
+                    if(stack1.peek()>stack2.peek()){
                         stack2.push(stack1.pop());
                     }
                     else{
@@ -29,14 +27,14 @@ public class Main {
                 }
             }
             stack1.push(stack2.pop());
-        }
-        while (!stack1.isEmpty()){
-            stack2.push(stack1.pop());
-        }
 
+            while (!stack2.isEmpty()){
+                stack1.push(stack2.pop());
+            }
+        }
         int i=0;
-        while (!stack2.isEmpty()){
-            nums[i] = stack2.pop();
+        while (!stack1.isEmpty()){
+            nums[i] = stack1.pop();
             i+=1;
         }
     }
